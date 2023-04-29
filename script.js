b2c941bcbc45d6ef8300d0b8b774d774a263a8fb
Jason_Evans <evans.jason@hotmail.co.uk>
2023-04-29 21:19:13 +0100

    pulling different data through depending on the weight condidtion

--- script.js
+++ script.js
@@ -4,26 +4,93 @@ const weightConditionEl = document.getElementById('weight-condition');
 const conditionTextEl = document.querySelector('.info-text');
 const motivationTextEl = document.getElementById('motivation-text');
 
-// put into its own function
-function randomQuote() {
-  fetch('data.json')
+// put into its own function obese
+async function obeseRandomQuote(condtion) {
+  fetch('data_quotes.json')
     .then((response) => response.json())
     .then((data) => {
-      // Do something with the data
-      const randNum = Math.floor(Math.random() * data.length);
-      console.log(randNum);
-      console.log(data[randNum].quote);
-      motivationTextEl.textContent = data[randNum].quote;
-    });
+      const obeseCount = data.obese_quotes.length;
+      const overweightCount = data.overweight_quotes.length;
+      const normalWeightCount = data.normalweight_quotes.length;
+      const underweightCount = data.underweight_quotes.length;
+
+      console.log(`Obese count: ${obeseCount}`);
+      console.log(`Overweight count: ${overweightCount}`);
+      console.log(`Normal weight count: ${normalWeightCount}`);
+      console.log(`Underweight count: ${underweightCount}`);
+
+      const randNum = Math.floor(Math.random() * obeseCount);
+      console.log(`Random_number: ${randNum}`);
+      motivationTextEl.textContent = data.obese_quotes[randNum].quote;
+    })
+    .catch((error) => console.error(error));
 }
 
-// fetch('quotes.json')
-//   .then((response) => response.json())
-//   .then((data) => {
-//     const randomIndex = Math.floor(Math.random() * data.length);
-//     const randomQuote = data[randomIndex];
-//     console.log(`Random Quote: "${randomQuote.quote}" - ${randomQuote.author}`);
-//   });
+// put into its own function overweight
+async function overweightRandomQuote() {
+  fetch('data_quotes.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const obeseCount = data.obese_quotes.length;
+      const overweightCount = data.overweight_quotes.length;
+      const normalWeightCount = data.normalweight_quotes.length;
+      const underweightCount = data.underweight_quotes.length;
+
+      console.log(`Obese count: ${obeseCount}`);
+      console.log(`Overweight count: ${overweightCount}`);
+      console.log(`Normal weight count: ${normalWeightCount}`);
+      console.log(`Underweight count: ${underweightCount}`);
+
+      const randNum = Math.floor(Math.random() * obeseCount);
+      console.log(`Random_number: ${randNum}`);
+      motivationTextEl.textContent = data.overweight_quotes[randNum].quote;
+    })
+    .catch((error) => console.error(error));
+}
+
+// put into its own function normal weight
+async function normalWeightRandomQuote() {
+  fetch('data_quotes.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const obeseCount = data.obese_quotes.length;
+      const overweightCount = data.overweight_quotes.length;
+      const normalWeightCount = data.normalweight_quotes.length;
+      const underweightCount = data.underweight_quotes.length;
+
+      console.log(`Obese count: ${obeseCount}`);
+      console.log(`Overweight count: ${overweightCount}`);
+      console.log(`Normal weight count: ${normalWeightCount}`);
+      console.log(`Underweight count: ${underweightCount}`);
+
+      const randNum = Math.floor(Math.random() * obeseCount);
+      console.log(`Random_number: ${randNum}`);
+      motivationTextEl.textContent = data.normalweight_quotes[randNum].quote;
+    })
+    .catch((error) => console.error(error));
+}
+
+// put into its own function under weigh
+async function underWeightRandomQuote() {
+  fetch('data_quotes.json')
+    .then((response) => response.json())
+    .then((data) => {
+      const obeseCount = data.obese_quotes.length;
+      const overweightCount = data.overweight_quotes.length;
+      const normalWeightCount = data.normalweight_quotes.length;
+      const underweightCount = data.underweight_quotes.length;
+
+      console.log(`Obese count: ${obeseCount}`);
+      console.log(`Overweight count: ${overweightCount}`);
+      console.log(`Normal weight count: ${normalWeightCount}`);
+      console.log(`Underweight count: ${underweightCount}`);
+
+      const randNum = Math.floor(Math.random() * obeseCount);
+      console.log(`Random_number: ${randNum}`);
+      motivationTextEl.textContent = data.underweight_quotes[randNum].quote;
+    })
+    .catch((error) => console.error(error));
+}
 
 function calcBmi() {
   const heightValue = document.getElementById('height').value / 100;
@@ -36,16 +103,16 @@ function calcBmi() {
 
   if (bmiValue < 18.5) {
     weightConditionEl.textContent = 'under weight';
-    randomQuote();
+    underWeightRandomQuote();
   } else if (bmiValue >= 18.5 && bmiValue < 24.9) {
     weightConditionEl.textContent = 'Normal weight';
-    randomQuote();
+    normalWeightRandomQuote();
   } else if (bmiValue >= 25 && bmiValue < 29.9) {
-    weightConditionEl.textContent = 'Over weight';
-    randomQuote();
+    weightConditionEl.textContent = 'Overweight';
+    overweightRandomQuote();
   } else if (bmiValue >= 30) {
     weightConditionEl.textContent = 'Obese';
-    randomQuote();
+    obeseRandomQuote();
   }
 }
 
